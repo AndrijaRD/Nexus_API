72dc36aa5e8849deab575687d431e6e4bb74ef95
AndrijaRD <radivojevandrija007@gmail.com>
2022-12-18 21:14:53 +0100

    gitignore

---
+++ functions/api.js
@@ -0,0 +1,17 @@
+import express, { Router } from "express";
+import serverless from "serverless-http";
+const fs = require('fs');
+
+const app = express();
+const router = Router();
+
+router.get("/:subject", (req, res) => {
+  var raw_data = fs.readFileSync('./src/' + req.params.subject + '.json');
+  var data = JSON.parse(raw_data);
+  res.send(data);
+});
+
+app.use(`/.netlify/functions/api`, router);
+
+export default app;
+export const handler = serverless(app);
